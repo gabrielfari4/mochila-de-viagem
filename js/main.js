a797b8089ff3160acfa6a1ff230d4c84c3574143
Gabriel Faria <gabrielfaria88@gmail.com>
2023-08-04 11:11:40 -0300

    inserindo item na lista

--- js/main.js
+++ js/main.js
@@ -2,8 +2,31 @@ const form = document.getElementById("novoItem")
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+  
+    
+    console.log()
+
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
 
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    //<li class="item"><strong>10</strong>Cuecas</li>
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+    
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem);
+
+    console.log(novoItem)
+}
