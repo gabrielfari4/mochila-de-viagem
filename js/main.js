4ce8c7e12d290a96ac19f1ec6233e2631df7ca41
Gabriel Faria <gabrielfaria88@gmail.com>
2023-08-04 11:45:41 -0300

    inserindo dados localstorage

--- js/main.js
+++ js/main.js
@@ -3,28 +3,29 @@ const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-  
-    
-    console.log()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+    
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) {
-    
-    //<li class="item"><strong>10</strong>Cuecas</li>
     const novoItem = document.createElement("li");
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = quantidade;
     
-
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += nome
    
-
     lista.appendChild(novoItem);
 
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("quantidade", quantidade);
     
 }
