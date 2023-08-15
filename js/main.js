397f2a84e0d7fed6cc7c99e766d032993ea7c64b
Gabriel Faria <gabrielfaria88@gmail.com>
2023-08-15 11:33:43 -0300

    deletando elemento

--- js/main.js
+++ js/main.js
@@ -54,6 +54,8 @@ function criaElemento(item) {
     numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta())
    
     lista.appendChild(novoItem);
 
@@ -62,6 +64,23 @@ function criaElemento(item) {
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+ item.id +"']").innerHTML = item.quantidade
 }
+
+function botaoDeleta() {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode)
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag) {
+    tag.remove()
+}
+
+
 
 //TODO: Aula 04.02
 
